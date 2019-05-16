4d942c96311593234f434e46c38e390b20fac388
Hicham <Taki.Hicham.01@outlook.com>
2019-05-16 11:38:37 +0200

    Add project files.

---
+++ APOD_UWP/MainPage.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace APOD_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        const string EndpointURL = "https://api.nasa.gov/planetary/apod";
+
+        // Settings name strings, used to preserve UI values between sessions.
+        const string SettingDateToday = "date today";
+        const string SettingShowOnStartup = "show on startup";
+        const string SettingImageCountToday = "image count today";
+        const string SettingLimitRange = "limit range";
+        // Declare a container for the local settings.
+        Windows.Storage.ApplicationDataContainer localSettings;
+
+
+
+        DateTime launchDate = new DateTime(1965, 6, 16);
+
+        int imageCountToday;
+
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MonthCalendar.MinDate = launchDate;
+            MonthCalendar.MaxDate = DateTime.Today;
+            localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            ReadSettings();
+        }
+
+        private void ReadSettings()
+        {
+            bool isToday = false;
+
+            Object todayObject = localSettings.Values[SettingDateToday];
+
+            if (todayObject != null)
+            {
+                DateTime dt = DateTime.Parse((string)todayObject);
+                isToday = dt.Equals(DateTime.Today);
+            }
+
+            imageCountToday = 0;
+
+            if (isToday)
+            {
+                Object value = localSettings.Values[SettingImageCountToday];
+                imageCountToday = value != null ? int.Parse((string)value) : imageCountToday;
+            }
+            ImagesTodayTextBox.Text = imageCountToday.ToString();
+
+            // Set the UI check boxes, depending on the stored settings or defaults if there are no settings.
+            Object showTodayObject = localSettings.Values[SettingShowOnStartup];
+            if (showTodayObject != null)
+            {
+                ShowTodaysImageCheckBox.IsChecked = bool.Parse((string)showTodayObject);
+            }
+            else
+            {
+                // Set the default.
+                ShowTodaysImageCheckBox.IsChecked = true;
+            }
+
+            Object limitRangeObject = localSettings.Values[SettingLimitRange];
+            if (limitRangeObject != null)
+            {
+                LimitRangeCheckBox.IsChecked = bool.Parse((string)limitRangeObject);
+            }
+            else
+            {
+                // Set the default.
+                LimitRangeCheckBox.IsChecked = false;
+            }
+
+            // Show today's image if the check box requires it.
+            if (ShowTodaysImageCheckBox.IsChecked == true)
+            {
+                MonthCalendar.Date = DateTime.Today;
+            }
+        }
+
+        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            LimitRangeCheckBox.IsChecked = false;
+            MonthCalendar.Date = launchDate;
+        }
+
+        private void LimitRangeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var firstDayOfThisYear = new DateTime(DateTime.Today.Year, 1, 1);
+            MonthCalendar.MinDate = firstDayOfThisYear;
+        }
+
+        private void LimitRangeCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MonthCalendar.MinDate = launchDate;
+        }
+
+        private async void MonthCalendar_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+            await RetrievePhoto();
+        }
+
+        bool isSupportedFormat(string photoURL)
+        {
+            string ext = Path.GetExtension(photoURL).ToLower();
+            string[] supportedExtensions = new string[] { ".jpg", ".jpeg", ".png", ".gif", ".tif" , ".bmp" , ".ico" , ".svg" };
+            return supportedExtensions.Contains(ext);
+        }
+
+        private async Task RetrievePhoto()
+        {
+            var client = new HttpClient();
+            JObject jResult = null;
+            string responseContent = null;
+            string description = null;
+            string photoUrl = null;
+            string copyright = null;
+
+            ImageCopyrightTextBox.Text = "NASA";
+            DescriptionTextBox.Text = "";
+
+            DateTimeOffset dt = (DateTimeOffset)MonthCalendar.Date;
+            string dateSelected = $"{dt.Year.ToString("00")}-{dt.Month.ToString("00")}-{dt.Day.ToString("00")}";
+            string URLParams = $"?date={dateSelected}&api_key=DEMO_KEY";
+
+            client.BaseAddress = new Uri(EndpointURL);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync(URLParams).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    responseContent = await response.Content.ReadAsStringAsync();
+
+                    jResult = JObject.Parse(responseContent);
+
+                    photoUrl = (string)jResult["url"];
+
+                    var photoURL = new Uri(photoUrl);
+                    var bmi = new BitmapImage(photoURL);
+
+                    ImagePictureBox.Source = bmi;
+                    if (isSupportedFormat(photoUrl))
+                    {
+                        // Get the copyright message, but fill with "NASA" if no name is provided.
+                        copyright = (string)jResult["copyright"];
+                        if (copyright != null && copyright.Length > 0)
+                        {
+                            ImageCopyrightTextBox.Text = copyright;
+                        }
+
+                        // Populate the description text box.
+                        description = (string)jResult["explanation"];
+                        DescriptionTextBox.Text = description;
+                    }
+                    else
+                    {
+                        DescriptionTextBox.Text = $"Image type is not supported. URL is {photoUrl}";
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    DescriptionTextBox.Text = $"Image data is not supported. {ex.Message}";
+                }
+
+                // Keep track of our downloads in case we reach the limit.
+                ++imageCountToday;
+                ImagesTodayTextBox.Text = imageCountToday.ToString();
+
+            }
+            else
+            {
+                DescriptionTextBox.Text = "We were unable to retrieve the NASA picture for that day: " +
+                    $"{response.StatusCode.ToString()} {response.ReasonPhrase}";
+            }
+
+        }
+
+        private void Grid_LostFocus(object sender, RoutedEventArgs e)
+        {
+            WriteSettings();
+        }
+
+        private void WriteSettings()
+        {
+            // Preserve the required UI settings in the local storage container.
+            localSettings.Values[SettingDateToday] = DateTime.Today.ToString();
+            localSettings.Values[SettingShowOnStartup] = ShowTodaysImageCheckBox.IsChecked.ToString();
+            localSettings.Values[SettingLimitRange] = LimitRangeCheckBox.IsChecked.ToString();
+            localSettings.Values[SettingImageCountToday] = imageCountToday.ToString();
+        }
+    }
+}
